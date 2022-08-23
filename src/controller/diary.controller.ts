a3908bc12e9659c7656c177ea44c38140dfd3732
BerkcanBalkaya <berkcanbalkaya2010@gmail.com>
2022-08-23 11:29:13 +0300

    Correcting CRUD operations

--- src/controller/diary.controller.ts
+++ src/controller/diary.controller.ts
@@ -7,24 +7,11 @@ import {
   UpdateDiaryInput,
 } from "../schema/diary.shcema";
 import {
+  checkTitleExistsForUser,
   createDiary,
   getAllDiaryById,
   getDiaryById,
 } from "../service/diary.service";
-
-// import { findUserById } from "../service/user.service";
-// TODO: mesela buarada userıd çekilirken önce id çekip bundan finduserbyid yapıyorum referans olarak çalışsın diye bunun başka bir yolu olamaz mı
-// TODO: mesela bizim app.use() olarak kullandığımız middleware yüzünden response içerisinde her seferinde user bilgileri gönderiliyor bu günvenlik açığı oluşturmaz mı
-// export async function getAllDiaryHandler(req: Request, res: Response) {
-//   const userId = res.locals.user._id;
-//   try {
-//     const user = await findUserById(userId);
-//     console.log(...user?.diary);
-//     return res.send(user);
-//   } catch (e) {
-//     res.status(500).send(e);
-//   }
-// }
 
 export async function getAllDiaryHandler(req: Request, res: Response) {
   const userId = res.locals.user._id;
@@ -47,17 +34,12 @@ export async function createDiaryHandler(
   console.log(body);
 
   try {
-    const diary = await createDiary(body);
-    console.log(diary.delete);
-
-    // diary!.user?.push(userId);
-    // diary.save()
-
-    // const user = await findUserById(userId);
-    // TODO: Burada mesela biz middleware ile user var mı yokmu kontrol etmemize rağmen user null olabilir diye bize uyarı veriyor bunu ! dışında çözebilir miyiz
-    // user!.diary.push(diary._id);
-    // user!.save();
-
+    if (await checkTitleExistsForUser(userId, body.title)) {
+      return res.send(
+        "Oluşturmaya çalıştığınız günlük başlığında başka bir günlüğünüz bulunmakta. Lütfen farklı bir başlık belirleyiniz."
+      );
+    }
+    await createDiary(body);
     return res.send("Başarılı bir şekilde günlük oluşturdunuz");
   } catch (e: any) {
     if (e.code === 11000) {
@@ -73,13 +55,20 @@ export async function deleteDiaryHandler(
   req: Request<DeleteDiaryInput>,
   res: Response
 ) {
-  const id = req.params.id;
-  const userId = res.locals._id;
+  const diaryId = req.params.id;
+  const userId = res.locals.user._id;
 
   try {
-    const diary = await getDiaryById(userId);
+    const diary = await getDiaryById(diaryId);
     // TODO: Hani null dönmesinin bana göre bir yolu yok ama yine de sormak istedim belki döner diye
-    if (id !== diary?.user?.toString() || null) {
+    if (!diary) {
+      return res.send(
+        "Lütfen bu id ye ait sahip bir diary'i olduğundan emin olun"
+      );
+    }
+    console.log(diaryId, userId);
+
+    if (userId !== diary?.user?.toString() || null) {
       return res.send(
         "Lütfen silmek istediğiniz diary'nin size ait olduğundan emin olun"
       );
@@ -99,10 +88,14 @@ export async function updateDiaryHandler(
 ) {
   const id = req.params.id;
   const { title, body } = req.body;
-  const userId = res.locals._id;
+  const userId = res.locals.user._id;
+
+  console.log(userId, id);
+
   try {
-    const diary = await getDiaryById(userId);
-    if (id !== diary?.user?.toString() || "") {
+    const diary = await getDiaryById(id);
+    // TODO: Burada han var mı yok mu emin olamadığımız için kullandığımız soru işaretleri yerine farklı bir yaklaşımla boş olamayacağını söyleyebilir miyiz
+    if (userId !== diary?.user?.toString() || "") {
       return res.send(
         "Lütfen güncellemek istediğiniz diary'nin size ait olduğundan emin olun"
       );
@@ -110,7 +103,12 @@ export async function updateDiaryHandler(
     diary!.title = title;
     diary!.body = body;
     diary?.save();
+    return res.send("Güncelleme işlemi başarılı bir şekilde gerçekleştirildi.");
   } catch (e) {
     return res.status(500).send(e);
   }
 }
+
+// TODO: 1)Bu kullanıcı bilgilerinin nasıl alındığına bir daha baktım ve bu bilgileri jwt token içerisinden alıyor muşuz biz.
+// * Peki o zaman kullanıcı bilgilerinin mesela admin olup olmadığı gibi jwt token içerisinde tutmamız ne kadar doğru bu token üzerinde oynama yapılıp kullanıcı kendisini admin gibi gösterip token atabilir mi
+// * Eğer atabiliyorsa bu benim tarafta güvenlik açığı doğurur.
