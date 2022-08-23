4086c1fef4c7c1a18180f059bb9e6999cfae2de2
BerkcanBalkaya <berkcanbalkaya2010@gmail.com>
2022-08-23 11:34:54 +0300

    Refactoring

--- src/routes/diary.routes.ts
+++ src/routes/diary.routes.ts
@@ -35,10 +35,3 @@ router.put(
 );
 
 export default router;
-
-// TODO: Sorulacaklar
-// * 1)yukarıdaki gibi routeları belirtmeme rağmen postmen hata veriyor
-// * 2)controller içerisinde update, delete için özel olarak servisler yazıp onlar üzerinden mi işlemleri yapmak mantıklı
-// *    findbyIdAndDelete ve update versiyonlarını kullanmak gibi yoksa benim yaptığım gibi olanı mı mantıklı id den bul sil veya id den bul fieldları doldur savele gibi
-// * 3)çoğu yerde diary nin boş gelmeyeceğini belirtmek için ! veya ? kullanmak zorunda kaldım bunun farklı bir yolu olabilir mi
-// * 4)
