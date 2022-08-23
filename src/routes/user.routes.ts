93fc7ff073a825a09dc9d865a5f8f51c071c8be7
BerkcanBalkaya <berkcanbalkaya2010@gmail.com>
2022-08-23 11:40:35 +0300

    WIP! trying to add assignrole path to router

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,14 +1,18 @@
 import express from "express";
 import {
+  assignRoleHandler,
   createUserHandler,
   forgotPasswordHandler,
   getCurrentUserHandler,
   resetPasswordHandler,
   verifyUserHandler,
 } from "../controller/user.controller";
+import requireAdmin from "../middleware/checkAdminExists";
+import checkAdminExists from "../middleware/checkAdminExists";
 import requireUser from "../middleware/requireUser";
 import validateResource from "../middleware/validateResourse";
 import {
+  assignRoleSchema,
   createUserSchema,
   forgotPasswordSchema,
   resetPasswordSchema,
@@ -39,6 +43,12 @@ router.post(
   "/api/users/resetpassword/:id/:passwordResetCode",
   validateResource(resetPasswordSchema),
   resetPasswordHandler
+);
+
+router.post(
+  "/api/admin/assignrole/:id",
+  [validateResource(assignRoleSchema), requireAdmin],
+  assignRoleHandler
 );
 
 router.get("/api/users/me", requireUser, getCurrentUserHandler);
