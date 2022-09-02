6526781bd10fd334d59b4e9576ef31c3772b972e
BerkcanBalkaya <berkcanbalkaya2010@gmail.com>
2022-09-02 13:26:58 +0300

    Refactoring

--- src/controller/diary.controller.ts
+++ src/controller/diary.controller.ts
@@ -1,9 +1,8 @@
 import { Request, Response } from "express";
-import { Diary } from "../model/diary.model";
-import UserModel, { User } from "../model/user.model";
 import {
   CreateDiaryInput,
   DeleteDiaryInput,
+  GetDiaryInput,
   UpdateDiaryInput,
 } from "../schema/diary.shcema";
 import {
@@ -16,10 +15,57 @@ import {
 export async function getAllDiaryHandler(req: Request, res: Response) {
   const userId = res.locals.user._id;
   try {
-    const body = await getAllDiaryById(userId);
-    res.send(body);
+    const diaries = await getAllDiaryById(userId);
+    if (diaries.length === 0) {
+      res.status(404).send({
+        message: "Size ait diary bulunmamakta.",
+        body: "",
+      });
+    }
+    res.send({
+      message: "Başarılı bir şekilde size ait olan tüm diaryleri getirdiniz.",
+      body: diaries,
+    });
   } catch (e) {
-    return res.status(404).send(e);
+    return res.status(500).send({
+      message: "Bir şeyler ters gitti.",
+      body: e,
+    });
+  }
+}
+
+export async function getDiaryHandler(
+  req: Request<GetDiaryInput>,
+  res: Response
+) {
+  const diaryId = req.params.id;
+  const userId = res.locals.user._id;
+
+  try {
+    const diary = await getDiaryById(diaryId);
+    if (!diary) {
+      return res.status(404).send({
+        message: "Lütfen bu id ye ait bir diary olduğuna emin olun",
+        body: "",
+      });
+    }
+    if (userId !== diary.user?.toString() || null) {
+      return res.status(403).send({
+        message:
+          "Lütfen incelemek istediğiniz diary'nin size ait olduğundan emin olun",
+        body: "",
+      });
+    }
+
+    return res.send({
+      message: "Başarılı bir şekilde istenilen diary'i getirdiniz.",
+      body: diary,
+    });
+  } catch (e) {
+    return res.status(500).send({
+      message: "Bir şeyler ters gitti.",
+      body: e,
+    });
   }
 }
 
@@ -29,25 +75,35 @@ export async function createDiaryHandler(
 ) {
   // * referans koparmak için yaptım ama başka yolu var mı acaba
   const body = Object.assign(req.body);
+  // console.log(body);
   const userId = res.locals.user._id;
   body.user = userId;
-  console.log(body);
 
   try {
     if (await checkTitleExistsForUser(userId, body.title)) {
-      return res.send(
-        "Oluşturmaya çalıştığınız günlük başlığında başka bir günlüğünüz bulunmakta. Lütfen farklı bir başlık belirleyiniz."
-      );
+      return res.status(409).send({
+        message:
+          "Oluşturmaya çalıştığınız günlük başlığında başka bir günlüğünüz bulunmakta. Lütfen farklı bir başlık belirleyiniz.",
+        body: body,
+      });
     }
-    await createDiary(body);
-    return res.send("Başarılı bir şekilde günlük oluşturdunuz");
+    const diary = await createDiary(body);
+    return res.send({
+      message: "Başarılı bir şekilde günlük oluşturdunuz",
+      body: diary,
+    });
   } catch (e: any) {
-    if (e.code === 11000) {
-      return res
-        .status(409)
-        .send("Oluşturulmaya çalışılan diary başlığı zaten bulunmakta");
-    }
-    return res.status(500).send(e);
+    // * Aşağıda eski modeldeki unique title mantığına göre hata döndüğünde yakalamamızı sağlayan catch mantığı var ama her kullanıcı kendi hesabında title 1 diye oluşturabilsin ve kullanıcılarının birbirnini kısıtlamaması için yukarıya farklı bir if kontrolü konuldu.
+    // if (e.code === 11000) {
+    //   return res.status(409).send({
+    //     message: "Oluşturulmaya çalışılan diary başlığı zaten bulunmakta",
+    //     body: "",
+    //   });
+    // }
+    return res.status(500).send({
+      message: "Bir şeyler ters gitti.",
+      body: e,
+    });
   }
 }
 
@@ -62,23 +118,30 @@ export async function deleteDiaryHandler(
     const diary = await getDiaryById(diaryId);
     // TODO: Hani null dönmesinin bana göre bir yolu yok ama yine de sormak istedim belki döner diye
     if (!diary) {
-      return res.send(
-        "Lütfen bu id ye ait sahip bir diary'i olduğundan emin olun"
-      );
+      return res.status(404).send({
+        message: "Lütfen bu id ye ait bir diaryi olduğundan emin olun",
+        body: diaryId,
+      });
     }
-    console.log(diaryId, userId);
+    // console.log(diaryId, userId);
 
     if (userId !== diary?.user?.toString() || null) {
-      return res.send(
-        "Lütfen silmek istediğiniz diary'nin size ait olduğundan emin olun"
-      );
+      return res.status(403).send({
+        message:
+          "Lütfen silmek istediğiniz diary'nin size ait olduğundan emin olun",
+        body: diaryId,
+      });
     }
     diary.delete();
-    return res.send(
-      `${diary.title} başlıklı günlüğünüz başarılı bir şekilde silindi`
-    );
+    return res.send({
+      message: `${diary.title} başlıklı günlüğünüz başarılı bir şekilde silindi`,
+      body: diary,
+    });
   } catch (e) {
-    return res.status(500).send(e);
+    return res.status(500).send({
+      message: "Bir şeyler ters gitti.",
+      body: e,
+    });
   }
 }
 
@@ -86,26 +149,40 @@ export async function updateDiaryHandler(
   req: Request<UpdateDiaryInput["params"], {}, UpdateDiaryInput["body"]>,
   res: Response
 ) {
-  const id = req.params.id;
+  const diaryId = req.params.id;
   const { title, body } = req.body;
   const userId = res.locals.user._id;
 
-  console.log(userId, id);
+  // console.log(userId, id);
 
   try {
-    const diary = await getDiaryById(id);
+    const diary = await getDiaryById(diaryId);
     // TODO: Burada han var mı yok mu emin olamadığımız için kullandığımız soru işaretleri yerine farklı bir yaklaşımla boş olamayacağını söyleyebilir miyiz
+    if (!diary) {
+      return res.send({
+        message: "Lütfen bu id ye ait bir diaryi olduğundan emin olun",
+        body: diaryId,
+      });
+    }
     if (userId !== diary?.user?.toString() || "") {
-      return res.send(
-        "Lütfen güncellemek istediğiniz diary'nin size ait olduğundan emin olun"
-      );
+      return res.send({
+        message:
+          "Lütfen güncellemek istediğiniz diary'nin size ait olduğundan emin olun",
+        body: diaryId,
+      });
     }
     diary!.title = title;
     diary!.body = body;
     diary?.save();
-    return res.send("Güncelleme işlemi başarılı bir şekilde gerçekleştirildi.");
+    return res.send({
+      message: "Güncelleme işlemi başarılı bir şekilde gerçekleştirildi.",
+      body: diary,
+    });
   } catch (e) {
-    return res.status(500).send(e);
+    return res.status(500).send({
+      message: e,
+      body: "",
+    });
   }
 }
 
