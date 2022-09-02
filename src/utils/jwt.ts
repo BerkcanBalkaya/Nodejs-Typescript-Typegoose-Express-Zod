7dd9551aebab6d7cb1b8df9411f234135f13095e
BerkcanBalkaya <berkcanbalkaya2010@gmail.com>
2022-09-02 13:33:52 +0300

    Refactoring

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -9,6 +9,11 @@ export function signJwt(
   const signinKey = Buffer.from(config.get<string>(keyName), "base64").toString(
     "ascii"
   );
+  // console.log(
+  //   config.get<string>(keyName) + "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
+  // );
+  // console.log(process.env);
+
   return jwt.sign(object, signinKey, {
     ...(options && options),
     algorithm: "RS256",
