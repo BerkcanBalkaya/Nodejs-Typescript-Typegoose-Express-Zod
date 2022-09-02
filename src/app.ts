a372c148751718456dbc0366c95bc9983875843f
BerkcanBalkaya <berkcanbalkaya2010@gmail.com>
2022-09-02 13:26:16 +0300

    Move app creation to utils/server

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,10 @@
 require("dotenv").config();
-import express from "express";
 import config from "config";
 import connectToDb from "./utils/connectToDb";
 import log from "./utils/logger";
-import router from "./routes";
-import deserializeUser from "./middleware/deserializeUser";
+import createServer from "./utils/server";
 
-const app = express();
-
-app.use(express.json());
-
-app.use(deserializeUser);
-
-app.use(router);
+const app = createServer();
 
 const port = config.get("port");
 app.listen(port, () => {
